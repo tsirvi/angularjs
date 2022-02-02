a10a79cf065375a9a19d5bcf2965b389de31491c
tsirvi <95852302+tsirvi@users.noreply.github.com>
2022-02-02 22:11:57 +0530

    Add files via upload

---
+++ angularjs/customer.js
@@ -0,0 +1,10 @@
+var myapp=angular.module("myapp",[]);
+myapp.controller("myctrl",function($scope,$http) 
+{
+     $http.get('https://raw.githubusercontent.com/tsirvi/angularjs/main/customer.json')
+    .success(function(response)
+    {
+        $scope.names=response.customerInformation; 
+    });
+    $scope.rowlimit=5;
+});
