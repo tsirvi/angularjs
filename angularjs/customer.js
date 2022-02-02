83065a70fde2d676d8ea3b772180be238354876f
tsirvi <95852302+tsirvi@users.noreply.github.com>
2022-02-02 22:11:11 +0530

    Delete customer.js

--- angularjs/customer.js
+++
@@ -1,9 +0,0 @@
-var myapp=angular.module("myapp",[]);
-myapp.controller("myctrl",function($scope,$http) 
-{
-    $http.get('https://raw.githubusercontent.com/tsirvi/angularjs/main/customer.json')
-    .success(function(response)
-    {
-        $scope.names=response.customerInformation; 
-    });
-});
